a159cd2128302b94d2decd50ce612698b689aff0
Yash <yashjhunjhunwala700@gmail.com>
2024-07-20 13:29:37 +0530

    Print Anagrams Together

---
+++ Day-5(20.07.24)/PrintAnagramsTogether.java
@@ -0,0 +1,26 @@
+class PrintAnagramsTogether {
+    public List<List<String>> Anagrams(String[] string_list) {
+        // Code here
+        HashMap<String,List<String>> anagramWordsMap=new HashMap<>();
+        for(int i=0;i<string_list.length;i++){
+            char[] tmp=string_list[i].toCharArray();
+            Arrays.sort(tmp);
+            String tempString=new String(tmp);
+            if(anagramWordsMap.containsKey(tempString)){
+                List<String> anagrams=anagramWordsMap.get(tempString);
+                anagrams.add(string_list[i]);
+                anagramWordsMap.put(tempString,anagrams);
+            }else{
+                List<String> anagrams=new ArrayList<>();
+                anagrams.add(string_list[i]);
+                anagramWordsMap.put(tempString,anagrams);
+            }
+        }
+        List<List<String>> result=new ArrayList<>();
+        for(String s:anagramWordsMap.keySet()){
+            result.add(anagramWordsMap.get(s));
+           // System.out.println(s+"         "+anagramWordsMap.get(s));
+        }
+        return result;
+    }
+}
