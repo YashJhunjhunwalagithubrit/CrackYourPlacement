9db22b57c74c294e642a199f7d336247b0313a79
Yash <yashjhunjhunwala700@gmail.com>
2024-07-22 18:14:58 +0530

    Allocate Minimum Pages

---
+++ Day-7(22.07.24)/AllocateMinimumPages.java
@@ -0,0 +1,59 @@
+class AllocateMinimumPages {
+    // Function to find minimum number of pages.
+    public static long findPages(int n, int[] arr, int m) {
+        // Your code here
+         if(n < m)
+        {
+            return -1;
+        }
+        
+        int low = arr[0];
+        int high = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            if(arr[i] > low)
+            {
+                low = arr[i];
+            }
+            high += arr[i];
+        }
+        
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            int students = countstudents(arr, mid);
+            if(students > m)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+    
+    public static int countstudents(int[] arr, int pages)
+    {
+        int students = 1;
+        long pagestudent = 0;
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(pagestudent + arr[i] <= pages)
+            {
+                pagestudent += arr[i];
+            }
+            else
+            {
+                students += 1;
+                pagestudent = arr[i];
+            }
+        }
+        
+        return students;
+    }
+};
