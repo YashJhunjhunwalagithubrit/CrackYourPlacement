2b6d5e6cd52fc187180d842bd844737b74d0a08e
Yash <yashjhunjhunwala700@gmail.com>
2024-07-23 20:41:55 +0530

    Middle of the Linked List

---
+++ Day-8(23.07.24)/MiddleoftheLinkedList.java
@@ -0,0 +1,11 @@
+class MiddleoftheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPtr=head;
+        ListNode fastPtr=head;
+        while(fastPtr!=null && fastPtr.next!=null){
+            slowPtr=slowPtr.next;
+            fastPtr=fastPtr.next.next;
+        }
+        return slowPtr;
+    }
+}
