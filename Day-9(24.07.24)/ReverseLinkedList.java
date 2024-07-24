56340d3d1f14464c676660d33069d807e1c9055c
Yash <yashjhunjhunwala700@gmail.com>
2024-07-24 22:35:34 +0530

    Reverse Linked List

---
+++ Day-9(24.07.24)/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            return head;
+        }
+        ListNode preNode=null;
+        ListNode currNode=head;
+        while(currNode!=null){
+            ListNode nextNode=currNode.next;
+            currNode.next=preNode;
+            preNode=currNode;
+            currNode=nextNode;
+        }
+        head=preNode;
+        return head;
+    }
+}
