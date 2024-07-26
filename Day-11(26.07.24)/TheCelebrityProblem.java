08cae9e958acfd9b707007a99521c32b9f9cb2fb
Yash <yashjhunjhunwala700@gmail.com>
2024-07-26 14:36:12 +0530

    The Celebrity Problem

---
+++ Day-11(26.07.24)/TheCelebrityProblem.java
@@ -0,0 +1,20 @@
+class TheCelebrityProblem {
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n) {
+        // code here
+        int idx = 0;
+    	for(int i = 0; i < n; i++) {
+    	    if(M[idx][i] == 1) {
+    	        idx = i;
+    	    }
+    	}
+    	
+    	for(int i = 0; i < n; i++) {
+    	    if(i != idx && (M[idx][i] == 1 || M[i][idx] == 0)) {
+    	        return -1;
+    	    }
+    	}
+    	
+    	return idx;
+    }
+}
