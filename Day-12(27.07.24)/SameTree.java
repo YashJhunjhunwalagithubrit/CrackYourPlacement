65dbc1db315952eab20f268f78463ba8fcfedcd3
Yash <yashjhunjhunwala700@gmail.com>
2024-07-27 21:21:50 +0530

    Same Tree

---
+++ Day-12(27.07.24)/SameTree.java
@@ -0,0 +1,22 @@
+class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()){
+            TreeNode first=queue.poll();
+            TreeNode second=queue.poll();
+            if(first==null && second==null){
+                continue;
+            }else if(
+                first==null || second==null || first.val!=second.val){
+                    return false;
+            }
+            queue.add(first.left);
+            queue.add(second.left);
+            queue.add(first.right);
+            queue.add(second.right);
+        }
+        return true;
+    }
+}
