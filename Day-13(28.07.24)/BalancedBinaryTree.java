ae58b8219cd867977aa2dbcade5a8810a7a4ba51
Yash <yashjhunjhunwala700@gmail.com>
2024-07-28 20:28:51 +0530

    Balanced Binary Tree

---
+++ Day-13(28.07.24)/BalancedBinaryTree.java
@@ -0,0 +1,11 @@
+class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        if(Math.abs(height(root.left)-height(root.right))>1) return false;
+        return isBalanced(root.left)&& isBalanced(root.right);
+    }
+    private int height(TreeNode root){
+        if(root==null) return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+}
