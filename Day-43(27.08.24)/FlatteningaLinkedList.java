5f2f9f81999521ed343f23813bc5f9a7f822f01b
Yash <yashjhunjhunwala700@gmail.com>
2024-08-27 02:09:24 +0530

    Flattening a Linked List

---
+++ Day-43(27.08.24)/FlatteningaLinkedList.java
@@ -0,0 +1,28 @@
+class FlatteningaLinkedList {
+    // Function to flatten a linked list
+    Node flatten(Node root) {
+        // code here
+        List<Integer> arr = new ArrayList<>();
+        
+        Node ptr = root;
+        while(ptr!=null){
+            Node subPtr = ptr.bottom;
+            while(subPtr != null){
+                arr.add(subPtr.data);
+                subPtr = subPtr.bottom;
+            }
+            arr.add(ptr.data);
+            ptr = ptr.next;
+        }
+        
+        Collections.sort(arr); 
+        
+        Node n = new Node(arr.get(0));
+        Node p = n;
+        for(int i=1; i<arr.size(); i++){
+            p.bottom = new Node(arr.get(i));
+            p = p.bottom;
+        }
+        return n;
+    }
+}
